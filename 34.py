ccc18a8ad0c12125ee2876d45e869f530e542476
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-01 11:21:46 -0800

    Add files via upload

---
+++ 34.py
@@ -0,0 +1,21 @@
+import sys
+i = 0
+tagged = ""
+encrypt = []
+for letter in sys.argv[1]:
+    if (i % 2 == 0 and i != 0):
+        tagged += letter
+    if (i % 3 == 0 and i != 0):
+        tagged += letter
+    if (len(tagged) == 2):
+        for letters in tagged:
+            encrypt.append(letters)
+        (lambda: print(chr(ord(encrypt[1]) + (ord(encrypt[0])-96)), end=""))()
+        encrypt = []
+        tagged = ""
+    elif(len(tagged) == 1):
+        continue
+    elif(len(tagged) == 0):
+        print(letter, end="")
+    i+=1
+    
